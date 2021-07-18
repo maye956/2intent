19332507fa9a6f6bd0bc284c77b70cec7a5f3866
pulidomayerlyn@gmail.com <pulidomayerlyn@gmail.com>
2021-07-18 19:49:00 -0300

    agregue una clase

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1111
+{
+    class Class1
+    {
+
+        public int numero;
+        public int numbre;
+
+    }
+}
